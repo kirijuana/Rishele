3ca244b748807bd57b6d22a175222ebfbf7771b5
kirijuana <sazkirand67@gmail.com>
2019-05-14 17:06:49 +0700

    Добавьте файлы проекта.

---
+++ KRIP_LAB6_RISHELE_SHARP/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace KRIP_LAB6_RISHELE_SHARP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShifrText = new System.Windows.Forms.TextBox();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.Text = new System.Windows.Forms.TextBox();
+            this.button_unShifr = new System.Windows.Forms.Button();
+            this.button_Shifr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ShifrText
+            // 
+            this.ShifrText.Location = new System.Drawing.Point(53, 239);
+            this.ShifrText.Name = "ShifrText";
+            this.ShifrText.Size = new System.Drawing.Size(413, 20);
+            this.ShifrText.TabIndex = 14;
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(25, 100);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(81, 20);
+            this.keyBox.TabIndex = 13;
+            // 
+            // Text
+            // 
+            this.Text.Location = new System.Drawing.Point(64, 34);
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(384, 20);
+            this.Text.TabIndex = 12;
+            // 
+            // button_unShifr
+            // 
+            this.button_unShifr.Location = new System.Drawing.Point(168, 129);
+            this.button_unShifr.Name = "button_unShifr";
+            this.button_unShifr.Size = new System.Drawing.Size(75, 23);
+            this.button_unShifr.TabIndex = 11;
+            this.button_unShifr.Text = "unShifr";
+            this.button_unShifr.UseVisualStyleBackColor = true;
+            this.button_unShifr.Click += new System.EventHandler(this.button_unShifr_Click);
+            // 
+            // button_Shifr
+            // 
+            this.button_Shifr.Location = new System.Drawing.Point(168, 75);
+            this.button_Shifr.Name = "button_Shifr";
+            this.button_Shifr.Size = new System.Drawing.Size(75, 23);
+            this.button_Shifr.TabIndex = 10;
+            this.button_Shifr.Text = "Shifr";
+            this.button_Shifr.UseVisualStyleBackColor = true;
+            this.button_Shifr.Click += new System.EventHandler(this.button_Shifr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 293);
+            this.Controls.Add(this.ShifrText);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.button_unShifr);
+            this.Controls.Add(this.button_Shifr);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ShifrText;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.TextBox Text;
+        private System.Windows.Forms.Button button_unShifr;
+        private System.Windows.Forms.Button button_Shifr;
+    }
+}
+
---
+++ KRIP_LAB6_RISHELE_SHARP/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KRIP_LAB6_RISHELE_SHARP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button_Shifr_Click(object sender, EventArgs e)
+        {
+            char[] text = Text.Text.ToCharArray();
+            char[] shifr = Text.Text.ToCharArray();
+            char[] key_text = keyBox.Text.ToCharArray();
+            int mas_size = key_text.Length;
+
+            for (int i = 0; i < key_text.Length; i++)
+            {
+                if(key_text[i] == '(' || key_text[i] == ')')
+                {
+                    mas_size--;
+                }
+            }
+            string key_temp = "\0\0"; int c = 0; char temp_key = ' ';
+
+            //     int[] key_mas = new int[mas_size];
+            int z = 0, plus = 0;
+            for (int i = 0; i < key_text.Length;)
+            {
+                if (z < text.Length) // если текст короче ключа
+                {
+                    if (key_text[i] == '(' || key_text[i] == ')' || key_text[i] == ' ')
+                    {
+                        
+                        if (key_text[i] == ')')
+                        {
+                            plus = z;
+                        }
+                        i++;
+                    }
+                    else
+                    {
+                        c = 0;
+                        while (key_text[i] != ' ' && key_text[i] != '(' && key_text[i] != ')')
+                        {
+                            temp_key = key_text[i];
+                            key_temp = key_temp.Remove(c, 1).Insert(c, temp_key.ToString());                      
+                            c++;
+                            i++;
+                        }
+
+                        shifr[z] = text[Convert.ToInt32(key_temp) + plus];
+                        z++;
+                        key_temp = key_temp.Remove(1, 1).Insert(1, "\0");
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+
+            string text1 = new string(shifr);
+            ShifrText.Text = text1;
+        }
+
+        private void button_unShifr_Click(object sender, EventArgs e)
+        {
+            char[] text = Text.Text.ToCharArray();
+            char[] shifr = Text.Text.ToCharArray();
+            char[] key_text = keyBox.Text.ToCharArray();
+            int mas_size = key_text.Length;
+
+            int z = 0, plus = 0;
+            string key_temp = "\0\0"; int c = 0; char temp_key = ' ';
+            for (int i = 0; i < key_text.Length && z < text.Length;)
+            {
+                if (z < text.Length) // если текст короче ключа
+                {
+                    if (key_text[i] == '(' || key_text[i] == ')' || key_text[i] == ' ')
+                    {
+
+                        if (key_text[i] == ')')
+                        {
+                            plus = z;
+                        }
+                        i++;
+                    }
+                    else
+                    {
+                        c = 0;
+                        while (key_text[i] != ' ' && key_text[i] != '(' && key_text[i] != ')')
+                        {
+                            temp_key = key_text[i];
+                            key_temp = key_temp.Remove(c, 1).Insert(c, temp_key.ToString());
+                            c++;
+                            i++;
+                        }
+                        text[Convert.ToInt32(key_temp) + plus] = shifr[z];
+                        key_temp = key_temp.Remove(1, 1).Insert(1, "\0");
+                        z++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string text1 = new string(text);
+            ShifrText.Text = text1;
+        }
+    }
+    }
